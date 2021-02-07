6c39828fb725d19b023471af52a86bb42d1c5569
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2021-02-07 18:43:07 -0500

    Add sample components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const OrderPie = ({ PieForm, orderAction }) => {
+
+  // ...assume fulsome pie data comes from another asyncronous call instead of being hard coded below
+  const pieData = {name: "Cherry"} 
+
+  const handleSubmit = () => {
+    orderAction(pieData)
+    // ...assume there's a following method to redirect to <OrderPreviewPage/>
+  }
+
+  return (
+    <PieForm
+      pieName={pieData.name}
+      onSubmit={handleSubmit}
+    />
+  )
+}
+
+const SimplePieForm = ({ pieName, onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <p>Click to confirm your order of the pie below</p>
+      <p>{pieName}</p>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+const postPieOrderService = (pieData) => {
+  // assume this posts pie data to complete the order instead of an alert
+  alert(`Ordering Pie: ${JSON.stringify(pieData)} `)
+}
+
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OrderPie
+        PieForm={SimplePieForm}
+        orderAction={(pieData) => postPieOrderService(pieData)}
+      />
     </div>
   );
 }
